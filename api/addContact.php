bcd42886050f9f0f114ad7e38eb82d77149f190e
SantiagoAl <44738682+SantiagoAl@users.noreply.github.com>
2020-01-24 19:19:16 -0500

    Create addContact.php

---
+++ api/addContact.php
@@ -0,0 +1,56 @@
+<?php
+    
+    $inData = getRequestInfo();
+    
+    $UID = $inData["UID"];
+    $firstName = $inData["firstName"];
+    $lastName = $inData["lastName"];
+    $email = $inData["email"];
+    $phoneNumber = $inData["phoneNumber"];
+    $notes = $inData["notes"];
+    
+    $conn = new mysqli("sql9.freemysqlhosting.net", "sql9319845", "l64JHb7YZj", "sql9319845", "3306");
+    
+    if ($conn->connect_error)
+    {
+        returnWithError($conn->connect_error);
+    }
+    else
+    {
+        $sql = "INSERT INTO Contacts (UID, firstName, lastName, email, phoneNumber, notes) VALUES ('$UID', '$firstName', '$lastName', '$email', '$phoneNumber', '$notes')";
+        
+        if ($result = $conn->query($sql) != true)
+        {
+            returnWithError($conn->error);
+        }
+        else
+        {
+            returnWithInfo($firstName, $lastName);
+        }
+        
+        $conn->close();
+    }
+    
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+    
+    function sendResultInfoAsJson($obj)
+    {
+        header('Content-type: application/json');
+        echo($obj);
+    }
+    
+    function returnWithInfo($firstName, $lastName)
+    {
+        $retValue = '{"Mess":' . "Success!" . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+        sendResultInfoAsJson($retValue);
+    }
+    
+    function returnWithError($err)
+    {
+        $retValue = '{"error":"' . $err . '"}';
+        sendResultInfoAsJson($retValue);
+    }
+?>
